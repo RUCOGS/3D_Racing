b0deba3460423b3756398760ce7e0cc0ec204b7a
ViggyNash <viggyviggynash@gmail.com>
2016-11-20 22:34:25 -0500

    temp commit

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -32,7 +32,20 @@ public class VehicleController : MonoBehaviour {
 	// START
 	void Start ()
     {
-        OrientVehicle();
+        // Calculate center of wheels
+        Vector3 centroid =
+            new Vector3(
+                (FLWheel.position.x + FRWheel.position.x + BLWheel.position.x + BRWheel.position.x) / 4,
+                (FLWheel.position.y + FRWheel.position.y + BLWheel.position.y + BRWheel.position.y) / 4,
+                (FLWheel.position.z + FRWheel.position.z + BLWheel.position.z + BRWheel.position.z) / 4
+            );
+
+        // Calculate up vector. Use arbitrary two distance vectors between three wheels.
+        Vector3 upOrient = Vector3.Cross(FRWheel.position - BRWheel.position, FRWheel.position - FLWheel.position);
+
+        // Position car object wrt centroid and up vector
+        Car.position = centroid;
+        Car.rotation = Quaternion.Slerp(Car.rotation, Quaternion.FromToRotation(Car.up, upOrient) * Car.rotation, .2f);
     }
 	
 	// UPDATE
@@ -43,19 +56,6 @@ public class VehicleController : MonoBehaviour {
 
     void OrientVehicle()
     {
-        // Calculate center of wheels
-        Vector3 centroid =
-            new Vector3(
-                (FLWheel.position.x + FRWheel.position.x + BLWheel.position.x + BRWheel.position.x) / 4,
-                (FLWheel.position.y + FRWheel.position.y + BLWheel.position.y + BRWheel.position.y) / 4,
-                (FLWheel.position.z + FRWheel.position.z + BLWheel.position.z + BRWheel.position.z) / 4
-            );
 
-        // Calculate up vector. Use arbitrary two distance vectors between three wheels.
-        Vector3 upOrient = Vector3.Cross( FRWheel.position - BRWheel.position, FRWheel.position - FLWheel.position);
-
-        // Position car object wrt centroid and up vector
-        Car.position = centroid;
-        Car.rotation = Quaternion.Slerp(Car.rotation, Quaternion.FromToRotation(Car.up, upOrient) * Car.rotation, .2f);
     }
 }
